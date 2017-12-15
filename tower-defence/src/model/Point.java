310831d018789b9320e053cbf7ac9d2dae4c3565
Onur Kocahan <32204545+kocahan@users.noreply.github.com>
2017-12-15 18:38:11 +0300

    Update Point.java

--- tower-defence/src/model/Point.java
+++ tower-defence/src/model/Point.java
@@ -6,6 +6,8 @@ public Point(int x , int y){
     row=x;
     column=y;
 }
+ public void setY(int y){column=y;}
+ public void setX(int x){row=x;}
 public int getX(){return row;}
 public int getY(){return column;}
 }
